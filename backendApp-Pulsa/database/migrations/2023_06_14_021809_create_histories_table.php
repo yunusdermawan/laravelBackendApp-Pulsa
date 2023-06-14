334621f60936612dbdda2c933708b1035d5d2ce3
naufal56-xtg <naufalseries56@gmail.com>
2023-06-14 09:27:24 +0700

    Perbaikan Relasi Tabel

--- backendApp-Pulsa/database/migrations/2023_06_14_021809_create_histories_table.php
+++ backendApp-Pulsa/database/migrations/2023_06_14_021809_create_histories_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('histories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('no_kartu', 100);
+            $table->string('provider', 20);
+            $table->integer('nominal');
+            $table->timestamp('tanggal');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('histories');
     }
 };
